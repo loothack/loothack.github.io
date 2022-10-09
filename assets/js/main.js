69f7bd94343f4a1ebfd335765d47069befae76c4
loothack <115432794+loothack@users.noreply.github.com>
2022-10-10 06:27:05 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function setLogin() {
+    var box = document.getElementById("login");
+    box.style.display = "block";
+}
+
+function tutup() {
+    var box = document.getElementById("login");
+    box.style.display = "none";
+}
